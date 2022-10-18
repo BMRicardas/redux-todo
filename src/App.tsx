47a02f8a17205472f298255d0895bfcdb331baac
BMRicardas <Ricardas.Brazdzius@gmail.com>
2022-10-18 14:30:21 +0300

    remove unnecessary vars after code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { Tabs } from 'components/Tabs/Tabs';
 import { TodoList } from 'components/Todo/TodoList/TodoList';
 import './App.scss';
 
-export const FORM_SIZE = 'massive';
-
-export const BUTTON_SIZE = 'large';
-
 export const App = () => {
   return (
     <Container>
