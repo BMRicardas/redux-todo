f2bb6cf16294712bb94e9e7c417e27c287182c22
BMRicardas <Ricardas.Brazdzius@gmail.com>
2022-10-18 15:17:23 +0300

    update redux folder and file structure

--- src/features/index.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { todoReducer } from './todo/reducers';
-
-export const reducers = combineReducers({
-  todo: todoReducer,
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
 
 import { App } from './App';
-import { reducers } from './features';
+import { store } from './store/rootReducer';
 
 import './styles/index.scss';
-
-const store = createStore(reducers, composeWithDevTools(applyMiddleware()));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
