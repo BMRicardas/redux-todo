c6125bc869a3cc2052640c1fcf4c39a89c14c83c
Ričardas Brazdžius <ricardas.barzdzius@gmail.com>
2022-10-19 11:05:42 +0300

    type selector

--- src/components/Todo/TodoList/TodoList.tsx
+++ src/components/Todo/TodoList/TodoList.tsx
@@ -1,15 +1,16 @@
 import { useSelector } from 'react-redux';
 
 import { selectFilteredTodos } from 'features/todo/selectors';
+import { Todo } from 'features/todo/reducers';
 
 import { TodoItem } from '../TodoItem/TodoItem';
 
 import './TodoList.scss';
 
 export const TodoList = () => {
-  const todos = useSelector(selectFilteredTodos);
+  const todos: Todo[] = useSelector(selectFilteredTodos);
 
-  const renderedList = todos.map((todo: any) => (
+  const renderedList = todos.map((todo) => (
     <TodoItem key={todo.id} id={todo.id} text={todo.text} done={todo.done} />
   ));
 
