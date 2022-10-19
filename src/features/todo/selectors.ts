0eb856a1f32ac64cb20730b9f08eb4a581adf97d
Ričardas Brazdžius <ricardas.barzdzius@gmail.com>
2022-10-19 11:16:13 +0300

    update filter selector return type

--- src/features/todo/selectors.ts
+++ src/features/todo/selectors.ts
@@ -1,10 +1,10 @@
 import { AppState } from 'index';
 
-import { Todo } from './reducers';
+import { Filter, Todo } from './reducers';
 
 export const selectTodos = (state: AppState): Todo[] => state.todo.todos;
 
-export const selectFilter = (state: AppState): string => state.todo.filter;
+export const selectFilter = (state: AppState): Filter => state.todo.filter;
 
 export const selectFilteredTodos = (state: AppState): Todo[] => {
   const filter = selectFilter(state);
